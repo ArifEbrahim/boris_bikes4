a7ea28da74d9be42a6a0273cac05a2587dde37dd
Stuart <stuartmcardle@gmail.com>
2021-04-28 14:13:53 +0100

    stage 8 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require "./lib/docking_station.rb"
 
 describe DockingStation do
-    describe ".release_bike" do
+    describe "release_bike" do
       it "releases bike" do 
         docking_station = DockingStation.new
         expect(docking_station.release_bike).to eq(Bike.new)
