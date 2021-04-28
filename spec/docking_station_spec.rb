f9047555f026ffe0839c20b274de84323d71de9f
Arif Ebrahim <>
2021-04-28 17:17:52 +0100

    added bike instance var

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,6 +8,17 @@ RSpec.describe DockingStation do
     expect(bike).to be_working 
   end
 
-  it {is_expected.to respond_to(:dock).with(1).argument }
+  # it {is_expected.to respond_to(:dock).with(1).argument }
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  # it { is_expected.to respond_to(:bike) }
+  it "returns docked bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
 end
