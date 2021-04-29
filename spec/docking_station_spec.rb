1d5c42c65af9800a639dac32b5ec42501fabf049
Arif Ebrahim <>
2021-04-29 18:53:43 +0100

    completed step 17

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,50 +1,50 @@
 require "./lib/docking_station.rb"
 
-
-
 RSpec.describe DockingStation do
-  it {is_expected.to respond_to(:release_bike) } 
-
-  it 'has a default capacity' do
-    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-  end
-  
-  describe "capacity check" do
-   subject { DockingStation.new(34) }
-   it 'has a variable capacity' do
-   expect(subject.capacity).to eq 34
-   end
-  end
-
   describe "#release_bike" do
-    it 'should release a working bike' do
+    it "releases a bike" do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.release_bike).to be_working 
+      expect(subject.release_bike).to eq bike
     end
 
-    it "throws an error when empty + release bike" do
-      expect {subject.release_bike}.to raise_error(RuntimeError)
+    it "raises an error when trying to release a bike when empty" do
+      expect { subject.release_bike }.to raise_error(RuntimeError)
+    end
+
+    it "releases a working bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to be_working
+    end
+  
+    it "returns a docked bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq(bike)
     end
   end
 
-  # it {is_expected.to respond_to(:dock).with(1).argument }
-  it "docks something" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to include(bike)
+  describe "#dock" do
+    it 'docks a bike' do 
+      bike = Bike.new
+      expect(subject.dock(bike)).to include(bike)
+    end
+
+    it "raises an error when trying to dock a bike when full" do
+      subject.capacity.times { subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error(RuntimeError)
+    end
   end
 
-  it 'throws an error when full' do
-    (subject.capacity).times { subject.dock(Bike.new) }
-    expect {subject.dock(Bike.new)}.to raise_error(RuntimeError)
-  end
+  describe "setting capacity" do
+    it "sets a default capacity of 20" do
+      expect(subject.capacity).to eq(DockingStation::DEFAULT_CAPACITY)
+    end
 
-
-  # it { is_expected.to respond_to(:bike) }
-  it "returns docked bike" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes).to include(bike)
+    let (:test) {DockingStation.new(34)}
+    it "has a variable capacity" do
+      expect(test.capacity).to eq(34) 
+    end
   end
 end
-
